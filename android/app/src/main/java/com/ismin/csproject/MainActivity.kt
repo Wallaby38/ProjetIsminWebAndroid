d56e0f5bfe14b1361d664a9fac44f24079b08693
Quentin <assassindu38qj@gmail.com>
2021-11-01 11:43:22 +0100

    Add menu

--- android/app/src/main/java/com/ismin/csproject/MainActivity.kt
+++ android/app/src/main/java/com/ismin/csproject/MainActivity.kt
@@ -2,6 +2,12 @@ package com.ismin.csproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.Window
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -19,4 +25,33 @@ class MainActivity : AppCompatActivity() {
         rcvBooks.layoutManager = LinearLayoutManager(this)
         rcvBooks.adapter = adapter
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.list -> {
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(this, "LISTE", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.map -> {
+                // User chose the "Favorite" action, mark the current item as a favorite...
+                Toast.makeText(this, "MAP", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.info -> {
+                // User chose the "Favorite" action, mark the current item as a favorite...
+                Toast.makeText(this, "INFO", Toast.LENGTH_SHORT).show()
+                true
+            }
+            // If we got here, the user's action was not recognized.
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
