e27961a2aca898935f3eebf88821d0641b7ba5ab
Quentin <assassindu38qj@gmail.com>
2021-11-02 13:57:24 +0100

    Add info of station in the detail view

--- android/app/src/main/java/com/ismin/csproject/DetailActivity.kt
+++ android/app/src/main/java/com/ismin/csproject/DetailActivity.kt
@@ -18,6 +18,14 @@ class DetailActivity : AppCompatActivity() {
 
         if (station != null) {
             findViewById<TextView>(R.id.id_station).text = station.id_station
+            findViewById<TextView>(R.id.acces_recharge).text = station.acces_recharge
+            findViewById<TextView>(R.id.nbre_pdc).text = station.nbre_pdc.toString()
+            findViewById<TextView>(R.id.region).text = station.region
+            findViewById<TextView>(R.id.type_prise).text = station.type_prise
+            findViewById<TextView>(R.id.ad_station).text = station.ad_station
+            findViewById<TextView>(R.id.type_prise).text = station.type_prise
+            findViewById<TextView>(R.id.puiss_max).text = station.puiss_max.toString()
+
         }
     }
 }
