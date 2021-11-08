8cffa90ab19b5bd442950268b25eee75b3a8b15e
Quentin <quentin.jayety@etu.emse.fr>
2021-11-08 15:22:30 +0100

    end of change detail/list station with api

--- android/app/src/main/java/com/ismin/csproject/DetailActivity.kt
+++ android/app/src/main/java/com/ismin/csproject/DetailActivity.kt
@@ -29,18 +29,10 @@ class DetailActivity : AppCompatActivity() {
 
         val id_station : String? = intent.getStringExtra("id_station")
 
-        //Toast.makeText(this,id.toString(), Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,id_station.toString(), Toast.LENGTH_SHORT).show()
 
-        if (id_station != null && station != null) {
+        if (id_station != null) {
             loadStation(id_station)
-            findViewById<TextView>(R.id.id_station).text = station!!.id_station
-            findViewById<TextView>(R.id.acces_recharge).text = station!!.acces_recharge
-            findViewById<TextView>(R.id.nbre_pdc).text = station!!.nbre_pdc.toString()
-            findViewById<TextView>(R.id.region).text = station!!.region
-            findViewById<TextView>(R.id.type_prise).text = station!!.type_prise
-            findViewById<TextView>(R.id.ad_station).text = station!!.ad_station
-            findViewById<TextView>(R.id.accessibilite).text = station!!.accessibilite
-            findViewById<TextView>(R.id.puiss_max).text = station!!.puiss_max.toString()
 
         }
     }
@@ -61,9 +53,21 @@ class DetailActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call<Station>, response: Response<Station>) {
                 station = response.body()
+                displayDetails()
             }
 
 
         })
+    }
+
+    fun displayDetails() {
+        findViewById<TextView>(R.id.id_station).text = station!!.id_station
+        findViewById<TextView>(R.id.acces_recharge).text = station!!.acces_recharge
+        findViewById<TextView>(R.id.nbre_pdc).text = station!!.nbre_pdc.toString()
+        findViewById<TextView>(R.id.region).text = station!!.region
+        findViewById<TextView>(R.id.type_prise).text = station!!.type_prise
+        findViewById<TextView>(R.id.ad_station).text = station!!.ad_station
+        findViewById<TextView>(R.id.accessibilite).text = station!!.accessibilite
+        findViewById<TextView>(R.id.puiss_max).text = station!!.puiss_max.toString()
     }
 }
